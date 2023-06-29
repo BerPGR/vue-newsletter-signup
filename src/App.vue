355568bc2317c865121792fefd77d2e6d9c5fd74
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-06-29 15:18:45 -0300

    Primary font as Roboto and cleaned the project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url('./styles.css');
+
+* {
+  font-family: 'Roboto', sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  min-height: 100vh;
+  background-color: hsl(235, 18%, 26%);
 }
 </style>
